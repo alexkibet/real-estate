3d026fcdc2d0818519c6f7f463de30922801bba8
alexkibet <akibetrio@gmail.com>
2019-09-26 18:40:21 +0300

    Updates

---
+++ precache-manifest.a2d5117dcfb9090e09e6171c70509ff9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b0b8fd1981d1ec2d0864fe889d956a",
+    "url": "/real-estate/index.html"
+  },
+  {
+    "revision": "dd91dd4131a7eccc81ce",
+    "url": "/real-estate/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8a0fc9af7a1eb8a04304",
+    "url": "/real-estate/static/css/main.462468e7.chunk.css"
+  },
+  {
+    "revision": "dd91dd4131a7eccc81ce",
+    "url": "/real-estate/static/js/2.24acf8fd.chunk.js"
+  },
+  {
+    "revision": "8a0fc9af7a1eb8a04304",
+    "url": "/real-estate/static/js/main.a9175dc6.chunk.js"
+  },
+  {
+    "revision": "58e9d228d271c700df63",
+    "url": "/real-estate/static/js/runtime~main.800e2e76.js"
+  },
+  {
+    "revision": "f9c0d89403ab4f9fd4a36d513d622ddf",
+    "url": "/real-estate/static/media/use.f9c0d894.jpeg"
+  }
+]);
